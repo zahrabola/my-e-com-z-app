4cbccad91a17c7aaafcb96a72e8fc67c64294d69
zahrabola <95353397+zahrabola@users.noreply.github.com>
2022-12-20 16:12:14 +0000

    Update
    Start app

--- src/App.js
+++ src/App.js
@@ -1,23 +1,16 @@
-import logo from './logo.svg';
+import {Browser as Router, Routes, Route} from "react-router-dom"
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Router>
+        <Routes>
+          <Route path="/" />
+          <Route path="/contact" />
+          <Route path="/cart" />
+        </Routes>
+      </Router>
     </div>
   );
 }
---
+++ src/data/Product.js
@@ -0,0 +1,129 @@
+import product1 from "./assets/img-1.png";
+import product2 from "./assets/img-2.png";
+import product3 from "./assets/img-3.png";
+import product4 from "./assets/img-4.png";
+import product5 from "./assets/img-5.png";
+import product6 from "./assets/img-6.png";
+import product7 from "./assets/img-7.png";
+import product8 from "./assets/img-8.png";
+import product9 from "./assets/img-9.png";
+import product10 from "./assets/img-10.png";
+import product11 from "./assets/img-11.png";
+import product12 from "./assets/img-12.png";
+import product13 from "./assets/img-13.png";
+import product14 from "./assets/img-14.png";
+import product15 from "./assets/img-15.png";
+
+export const Products = [
+  {
+    id: 1,
+    title: "Laptop",
+    productName: "Apple MacBook air laptop",
+    price: 959.0,
+    productImage: product1,
+  },
+  {
+    id: 2,
+    title: "Laptop",
+    productName: "SAMSUNG Galaxy laptop",
+    price: 1999.0,
+    productImage: product2,
+  },
+  {
+    id: 3,
+    title: "Laptop",
+    productName: "HP Pavilion laptop",
+    price: 999.0,
+    productImage: product3,
+  },
+  {
+    id: 4,
+    title: "Mobile",
+    productName: "iPhone 14 Plus",
+    price: 929.0,
+    productImage: product4,
+  },
+  {
+    id: 5,
+    title: "Mobile",
+    productName: "SAMSUNG Galaxy Z fold4",
+    price: 1649.0,
+    productImage: product5,
+  },
+  {
+    id: 6,
+    title: "Mobile",
+    productName: "Google Pixel 7 Pro",
+    price: 849.0,
+    productImage: product6,
+  },
+  {
+    id: 7,
+    title: "Watch",
+    productName: "Apple watch ",
+    price: 259.0,
+    productImage: product7,
+  },
+  {
+    id: 8,
+    title: "Watch",
+    productName: "SAMSUNG Galaxy watch",
+    price: 149.0,
+    productImage: product8,
+  },
+  {
+    id: 9,
+    title: "Watch",
+    productName: "Google Pixel watch",
+    price: 339.0,
+    productImage: product9,
+  },
+
+  {
+    id: 10,
+    title: "Tablet",
+    productName: "Apple iPad",
+    price: 499.0,
+    productImage: product10,
+  },
+  {
+    id: 11,
+    title: "Tablet",
+    productName: "SAMSUNG Galaxy Tab",
+    price: 219.0,
+    productImage: product11,
+  },
+  {
+    id: 12,
+    title: "Tablet",
+    productName: "Surface Pro 9 ",
+    price: 1599.0,
+    productImage: product12,
+  },
+  {
+    id: 13,
+    title: "Headphones",
+    productName: "Apple airpods",
+    price: 249.0,
+    productImage: product13,
+  },
+  {
+    id: 14,
+    title: "Headphones",
+    productName: "Galaxy Buds2",
+    price: 139.0,
+    productImage: product14,
+  },
+  {
+    id: 15,
+    title: "Headphones",
+    productName: "Beats Fit Pro",
+    price: 199.0,
+    productImage: product15,
+  },
+];
+
+////https://freefrontend.com/css-product-cards/
+///https://codepen.io/sam_garcia2/pen/PozpPRy
+///https://codepen.io/cupofmint/pen/wgyrRX
+//https://github.com/machadop1407/shopping-cart-react/blob/main/src/products.js
